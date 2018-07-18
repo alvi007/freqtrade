2e035598ced424c9e827a743ffd9343723bb438e
Matthias <xmatthias@outlook.com>
2018-07-18 21:31:19 +0200

    remove @abstractmethod as this method may not be present in new strategies

--- freqtrade/strategy/interface.py
+++ freqtrade/strategy/interface.py
@@ -51,7 +51,6 @@ class IStrategy(ABC):
     def __init__(self, config: dict) -> None:
         self.config = config
 
-    @abstractmethod
     def populate_indicators(self, dataframe: DataFrame) -> DataFrame:
         """
         Populate indicators that will be used in the Buy and Sell strategy
@@ -62,7 +61,6 @@ class IStrategy(ABC):
                       DeprecationWarning)
         return dataframe
 
-    @abstractmethod
     def populate_buy_trend(self, dataframe: DataFrame) -> DataFrame:
         """
         Based on TA indicators, populates the buy signal for the given dataframe
@@ -74,7 +72,6 @@ class IStrategy(ABC):
         dataframe.loc[(), 'buy'] = 0
         return dataframe
 
-    @abstractmethod
     def populate_sell_trend(self, dataframe: DataFrame) -> DataFrame:
         """
         Based on TA indicators, populates the sell signal for the given dataframe
